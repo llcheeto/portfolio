9d6869749219cc7713c3126a32092f463994fd7a
llcheeto <llcheeto@github.com>
2022-04-01 16:52:23 -0400

    added side nav/on click close

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import Topbar from "./components/topbar/Topbar";
 import React from 'react';
 import Intro from "./components/intro/Intro";
@@ -5,12 +6,16 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 import"./app.scss";
+import { useState } from "react";
 
 function App() {
+  const [menuOpen,setMenuOpen] = useState(true)
   return (
     <div className='app'>
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,30 +3,32 @@ import React from 'react';
 import {Person, Mail} from "@material-ui/icons";
 
 
-export default function Topbar() {
+export default function Topbar( {menuOpen, setMenuOpen}) {
     return (
-        <div className='topbar'>
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro" className="logo">Genius.</a>
-                        <div className="itemContainer">
-                            <Person/>
-                            <span>+(863)-214-7285</span>
-                        </div>
-                        <div className="itemContainer">
-                            <Mail/>
-                            <span>Briancornine@gamil.com</span>
-                        </div>
-                    </div>                
-                        <div className="right">
-                            <div className="hamburger">
-                                <span className="line1"></span>
-                                <span className="line2"></span>
-                                <span className="line3"></span>
-                            </div>    
-                        </div>
+                    <a href="#intro" className="logo">
+                        Genius.
+                    </a>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>+(863)-214-7285</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>Briancornine@gamil.com</span>
+                    </div>
+                </div>                
+                    <div className="right">
+                        <div className="hamburger" onClick={() =>setMenuOpen(!menuOpen)}>
+                            <span className="line1"></span>
+                            <span className="line2"></span>
+                            <span className="line3"></span>
+                        </div>    
                     </div>
                 </div>
+            </div>
     );
 }
 
