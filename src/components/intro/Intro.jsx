7559071d84622fbb38c1f087c2bfb68e7e527f6b
llcheeto <llcheeto@github.com>
2022-04-01 18:14:14 -0400

    finished landing page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import './intro.scss'
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
-        </div>
+            <div className="left">
+                <div className="imgContainter">
+                    <img src="assets/man.png" alt=""></img>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm </h2>
+                    <h1>Brian Cornine</h1>
+                    <h3>Freelance<span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/downarrow.png" alt="">
+                </img></a> 
+            </div>
+        </div>    
     )
 }
