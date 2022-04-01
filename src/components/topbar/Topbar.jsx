602091d0763ba004503ce18ee605d54ba463c4b3
llcheeto <llcheeto@github.com>
2022-04-01 13:03:48 -0400

    added nav-bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,18 +1,32 @@
 import './topbar.scss'
 import React from 'react';
+import {Person, Mail} from "@material-ui/icons";
+
 
 export default function Topbar() {
     return (
         <div className='topbar'>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro">aaaaa</a>
-                <div className="right">
-
+                    <a href="#intro" className="logo">Genius.</a>
+                        <div className="itemContainer">
+                            <Person/>
+                            <span>+(863)-214-7285</span>
+                        </div>
+                        <div className="itemContainer">
+                            <Mail/>
+                            <span>Briancornine@gamil.com</span>
+                        </div>
+                    </div>                
+                        <div className="right">
+                            <div className="hamburger">
+                                <span className="line1"></span>
+                                <span className="line2"></span>
+                                <span className="line3"></span>
+                            </div>    
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-        </div>
     );
 }
 
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Works() {
     return (
-        <div className="works">
+        <div className="works" id="works">
 
         </div>
     );
