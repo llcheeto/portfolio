7b605e77d394454182b43c443271a8c79da59d27
llcheeto <llcheeto@github.com>
2022-04-02 15:43:06 -0400

    added portfolio page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
-import './intro.scss'
+import './intro.scss';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Intro() {
+    const textRef = useRef();
+    
+    useEffect(() => {
+        init(textRef.current, {  
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 50,
+            strings: ["Developer", "Designer", "Content Creator"],
+        });  
+    },  []);
+    
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,7 +25,7 @@ export default function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm </h2>
                     <h1>Brian Cornine</h1>
-                    <h3>Freelance<span></span></h3>
+                    <h3>Freelance <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/downarrow.png" alt="">
