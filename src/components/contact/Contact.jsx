a9d5ac11dbcb5939ceeafb4f3ef8469594267fb9
llcheeto <llcheeto@github.com>
2022-04-05 14:55:58 -0400

    finished contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import {useState ,React} from 'react'
 import './contact.scss'
 
 export default function Contact() {
+    const [message, setMessage] = useState(false)
+
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        setMessage(true);
+    }
     return (
         <div className="contact" id="contact">
-
+            <div className="left">
+                <img src="assets/code.jpg" alt="code" />
+            </div>
+            <div className="right">
+                <h2>Contact</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email"></input>
+                    <textarea placeholder="Message"></textarea> 
+                    <button type="submit">Send</button>  
+                    {message && <span>Thanks, I'll respond shortly</span>}       
+                </form>
+            </div>
         </div>
     );
 }
